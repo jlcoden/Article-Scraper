aee4168cdfdb9a615fc41bcb10fa3f646adb30a7
Josh Cosson <joshlcosson@gmail.com>
2020-01-20 14:32:38 -0800

    Updated app so that saved articles can be deleted and added am modal to add notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,8 +2,14 @@ var express = require("express");
 // var request = require("request");
 var cheerio = require("cheerio");
 var db = require("../models");
-var app = express.Router();
+// var app = express.Router();
+var app = express();
 var axios = require("axios");
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
 
 module.exports = function(app) {
   //render homepage of found articles
@@ -17,49 +23,66 @@ module.exports = function(app) {
 
   app.get("/scrape", function(req, res) {
     // First, we grab the body of the html with axios
-    axios.get("https://www.npr.org/sections/news/").then(function(response) {
+    axios.get("https://www.npr.org/sections/news/").then(function(req) {
       // Then, we load that into cheerio and save it to $ for a shorthand selector
-      var $ = cheerio.load(response.data);
+      var $ = cheerio.load(req.data);
 
       //grab article divs from npr website
       $("article").each(function(i, element) {
         // Save an empty result object
         var result = {};
 
-        var title = $(element)
+        result.title = $(this)
           .find("h2")
           .text();
 
-        var link = $(element)
+        result.link = $(this)
           .find("h2")
           .children()
           .attr("href");
 
-        var summary = $(element)
+        result.summary = $(this)
           .find("p.teaser")
           .children()
           .text();
 
-        //if title, summary, & link included
-        if (title && summary && link) {
-          // check if the article has already been added
-          db.Article.find({ title }, function(err, data) {
-            if (err) {
-              res.status(404).send(err.toString());
-            }
-            if (data.length === 0) {
-              db.Article.create({
-                title,
-                summary,
-                link
-              });
-            }
+        // Create a new Article using the `result` object built from scraping
+        db.Article.create(result)
+          .then(function(dbArticle) {
+            // View the added result in the console
+            console.log(dbArticle);
+          })
+          .catch(function(err) {
+            // If an error occurred, log it
+            console.log(err);
           });
-        }
       });
+
+      // Send a message to the client
       res.redirect("/");
     });
   });
+
+  //       //if title, summary, & link included
+  //       if (result.title && result.summary && result.link) {
+  //         // check if the article has already been added
+  //         db.Article.find({ title }, function(err, data) {
+  //           if (err) {
+  //             res.status(404).send(err.toString());
+  //           }
+  //           if (data.length === 0) {
+  //             db.Article.create({
+  //               title,
+  //               summary,
+  //               link
+  //             });
+  //           }
+  //         });
+  //       }
+  //     });
+  //     res.redirect("/");
+  //   });
+  // });
 
   app.get("/articles", function(req, res) {
     // Grab every document in the Articles collection
@@ -74,7 +97,6 @@ module.exports = function(app) {
       });
   });
 
-  // Route for grabbing a specific Article by id, populate it with it's note
   app.get("/articles/:id", function(req, res) {
     // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
     db.Article.findOne({ _id: req.params.id })
@@ -90,10 +112,36 @@ module.exports = function(app) {
       });
   });
 
+  app.get("/note", function(req, res) {
+    // Find all Notes
+    db.Note.find({})
+      .then(function(dbNote) {
+        // If all Notes are successfully found, send them back to the client
+        res.json(dbNote);
+      })
+      .catch(function(err) {
+        // If an error occurs, send the error back to the client
+        res.json(err);
+      });
+  });
+
+  // CHANGE STATUS TO SAVED
+  app.put("/articles/:id", function(req, res) {
+    var id = req.params.id;
+    db.Article.update(
+      { _id: id },
+      { $set: { saved: req.body.saved } },
+      function(result) {
+        res.status(200).json({ message: "update saved status" });
+      }
+    );
+  });
+
   // Route for saving/updating an Article's associated Note
   app.post("/articles/:id", function(req, res) {
     // Create a new note and pass the req.body to the entry
     db.Note.create(req.body)
+
       .then(function(dbNote) {
         // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
         // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
@@ -114,17 +162,20 @@ module.exports = function(app) {
       });
   });
 
-  app.put("/articles/:id", function(req, res) {
-    var id = req.params.id;
-    db.Article.update(
-      { _id: id },
-      { $set: { saved: req.body.saved } },
-      function(result) {
-        res.status(200).json({ message: "changed saved status" });
-      }
-    );
+  app.put("/article/delete/:id", function(req, res) {
+    db.Article.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: { saved: false } }
+    )
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
   });
 
+  //KEEP THIS API ROUTE
   app.get("/saved", function(req, res) {
     db.Article.find({}, function(error, exists) {
       res.render("saved", {
--- server.js
+++ server.js
@@ -1,31 +1,33 @@
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-var axios = require("axios");
-var cheerio = require("cheerio");
-var db = require("./models");
+// var axios = require("axios");
+// var cheerio = require("cheerio");
+// var db = require("./models");
 var app = express();
+var path = require("path");
 var PORT = process.env.PORT || 8000;
 app.use(logger("dev"));
-var exphbs = require("express-handlebars");
 // var MONGODB_URI =
 //   "mongodb://heroku_nv5tggdw:Dasani1987@ds059661.mlab.com:59661/heroku_nv5tggdw";
 
 // Middleware
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-console.log(__dirname + "/public/img");
 app.use(express.static(__dirname + "/public/img"));
 require("dotenv").config();
 
 // Handlebars
+var exphbs = require("express-handlebars");
 app.engine(
   "handlebars",
   exphbs({
-    defaultLayout: "main"
+    defaultLayout: "main",
+    partialsDir: path.join(__dirname, "/views/layouts/partials")
   })
 );
+
 app.set("view engine", "handlebars");
 
 // Routes
@@ -35,7 +37,7 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false })); // for form submissions
 
 var MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+  process.env.MONGODB_URI || "mongodb://localhost/articleScraper";
 
 // Connect to the Mongo DB
 mongoose.connect(MONGODB_URI);
